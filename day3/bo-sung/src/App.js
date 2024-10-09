52de7d9e29007521f85b35c051a8f01156351e1d
Manh <manhamsterdam2003@gmail.com>
2024-10-09 10:36:43 +0700

    pc

--- day3/bo-sung/src/App.js
+++ day3/bo-sung/src/App.js
@@ -1,24 +1,85 @@
-import { useState } from "react";
-import Children from "./components/ConApp";
-function App() {
-  const [count, setCount] = useState(0);
+// import { useState } from "react";
+// import Children from "./components/ConApp";
+// function App() {
+//   const [count, setCount] = useState(0);
 
-  const handleIncrease = () => {
-    setCount(count + 1);
-  };
-  const handleDecrease = () => {
-    if (count === 0) return;
-    setCount(count - 1);
-  };
+//   const handleIncrease = () => {
+//     setCount(count + 1);
+//   };
+//   const handleDecrease = () => {
+//     if (count === 0) return;
+//     setCount(count - 1);
+//   };
+
+//   return (
+//     <div className="container">
+//       {/* <h1>Count :{count}</h1>
+//       <button onClick={handleIncrease}>Increase</button>
+//       <button onClick={handleDecrease}>Decrease</button> */}
+//       <Children props={{ count, handleIncrease, handleDecrease }} />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+// UseContext example
+import { useState, createContext, useContext } from "react";
+import ReactDOM from "react-dom/client";
+
+// Tạo UserContext
+const UserContext = createContext();
+
+function Component1() {
+  const [user, setUser] = useState("Jesse Hall");
 
   return (
-    <div className="container">
-      {/* <h1>Count :{count}</h1>
-      <button onClick={handleIncrease}>Increase</button>
-      <button onClick={handleDecrease}>Decrease</button> */}
-      <Children props={{ count, setCount, handleIncrease, handleDecrease }} />
-    </div>
+    <UserContext.Provider value={user}>
+      <h1>{`Hello ${user}!`}</h1>
+      <Component2 />
+    </UserContext.Provider>
   );
 }
 
-export default App;
+function Component2() {
+  return (
+    <>
+      <h1>Component 2</h1>
+      <Component3 />
+    </>
+  );
+}
+
+function Component3() {
+  return (
+    <>
+      <h1>Component 3</h1>
+      <Component4 />
+    </>
+  );
+}
+
+function Component4() {
+  return (
+    <>
+      <h1>Component 4</h1>
+      <Component5 />
+    </>
+  );
+}
+
+function Component5() {
+  const user = useContext(UserContext);
+
+  return (
+    <>
+      <h1>Component 5</h1>
+      <h2>{`Hello ${user} again!`}</h2>
+    </>
+  );
+}
+
+ 
+export default Component1;
--- day3/bo-sung/src/index.js
+++ day3/bo-sung/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Component1 from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Component1 />
   </React.StrictMode>
 );
 
